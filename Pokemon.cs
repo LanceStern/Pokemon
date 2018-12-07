d00c20588c346189eb3c020a58eec989b9fbbddd
LanceStern <dansmith2455@outlook.com>
2018-12-06 16:03:40 -0800

    Adding Pokemon class

---
+++ Pokemon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Pokemon
+{
+    public class Pokemon
+    {
+        public Pokemon()
+        {
+            Nickname = Name;
+        }
+
+        public Pokemon(string name, string nickname)
+        {
+            Name = name;
+            Nickname = nickname;
+        }
+
+        /// <summary>
+        /// Gets and sets the name of the Pokemon
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets and sets the nickname of the Pokemon
+        /// </summary>
+        public string Nickname { get; set; }
+
+    }
+}
